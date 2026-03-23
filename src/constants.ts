7a7cf07f50f8b117cffe7d4ecfa3f67961fbbc45
lionel518 <lionel518@users.noreply.github.com>
2026-03-23 23:02:08 +0800

    删除首页欢迎内容，添加 RSS 订阅到社交媒体

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@ import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import IconRss from "@/assets/icons/IconRss.svg";
 import { SITE } from "@/config";
 
 interface Social {
@@ -28,6 +29,12 @@ export const SOCIALS: Social[] = [
     href: "https://t.me/FreePeriodical",
     linkTitle: `${SITE.title} on Telegram`,
     icon: IconTelegram,
+  },
+  {
+    name: "RSS",
+    href: "/rss.xml",
+    linkTitle: `${SITE.title} RSS Feed`,
+    icon: IconRss,
   },
 ] as const;
 
